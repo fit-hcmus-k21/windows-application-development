68fc2099964385a4be0ec432a8ae483282f1b2e0
thanhngan22 <nganbui9a2017@gmail.com>
2023-09-24 11:37:50 +0700

    update solution and add demo task

---
+++ Week01/ConsoleApp1/ConsoleApp2/Program.cs
@@ -0,0 +1,123 @@
+﻿using System.Security.Cryptography;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Nguyên thủy - value
+            var start = 1; // ==> boxing
+            var end = 10;
+            var sum = 0; // j
+
+            // if, for, do, while
+            sum = calcSum(start, end);
+
+            Console.WriteLine($"Tong tu {start} den {end} la:{sum}");
+                
+
+            var message = start.ToString() + " $";
+
+            var a = new int[4];
+
+            try
+            {
+                a[0] = 1;
+                a[1] = 2;
+                a[2] = 3;
+                a[3] = 4;
+                //a[4] = 1;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Cannot operate: reason {ex.Message}");
+                // Log / Debug
+            }
+
+            var primes = new int[] { 2, 3, 5, 7, 11, 13 };
+            int[] squares = { 4, 9, 16, 25 };
+
+            string[] colors = { "red", "green", "blue" , "magenta", "yellow", "white"};
+            var line = new string[] // new Collection / new Arraylist
+            {
+                "straight", "curved", "foldable"
+            };
+
+            var rng = new Random(); // random number generator / dob = date of birth / gpa= 
+            int i = rng.Next();
+            int limit = rng.Next(100);
+            int range = rng.Next(20, 50);
+
+            int index = rng.Next(colors.Length);
+            string selectedColor = colors[index];
+
+
+            // string interpolation
+            //for(int i = 0; i < a.Length; i++)
+            //{
+            //    Console.Write($"{a[i]} ");
+            //}
+        }
+
+        /// <summary>
+        /// Kiểm tra một số có phải số nguyên tố hay không
+        /// </summary>
+        /// <param name="number">Số cần kiểm tra (>2)</param>
+        /// <returns>Kết quả kiểm tra</returns>
+        static bool IsPrime(int number) // checkPrime
+        {
+            bool result = true;
+
+            if (number < 2)
+            {
+                result = false;
+            } else
+            {
+                    
+            }
+
+            return result;
+        }
+
+        // hasAllOddDigits - isAllOddDigits
+        bool isAllOddDigits(int number)
+        {
+            bool result = true; // goto
+
+            // Đọc dễ hiểu
+            // Dễ debug
+            while (number > 0)
+            {
+                int digit = number % 10;
+                if (digit % 2 == 0)
+                {
+                    result = false;
+                    break;
+                }
+                number /= 10;
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Tính tổng dãy số
+        /// </summary>
+        /// <param name="start">Số bắt đầu</param>
+        /// <param name="end">Số kết thúc</param>
+        /// <returns></returns>
+        private static int calcSum(int start, int end)
+        {
+            int sum = 0;
+
+            for (int i = start; i <= end; i++)
+            {
+                sum += i;
+            }
+
+            return sum;
+        }
+
+    }
+}
---
+++ Week01/ConsoleApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void changeButton_Click(object sender, RoutedEventArgs e)
+        {
+            Random rng = new Random();
+
+            string[] salutes =
+            {
+                "Hello",
+                "Salute",
+                "Bon jour",
+                "Xiao",
+                "Selamat datang",
+                "Ohayo"
+            };
+            int i = rng.Next(salutes.Length);
+            string salute = salutes[i];
+            saluteLabel.Content = salute;
+
+            string[] avatars =
+            {
+                "Images/avatar01.jpg",
+                "Images/avatar02.jpg",
+                "Images/avatar03.jpg",
+                "Images/avatar04.jpg",
+                "Images/avatar05.jpg",
+                "Images/avatar06.jpg",
+                "Images/avatar07.jpg",
+                "Images/avatar08.jpg",
+                "Images/avatar09.jpg",
+                "Images/avatar10.jpg",
+            };
+            
+            i = rng.Next(avatars.Length);
+            string avatar = avatars[i];
+
+            var bitmap = new BitmapImage(
+                new Uri(avatar, UriKind.Relative)
+            ); // Unique resource identifier
+            avatarImage.Source = bitmap;
+        }
+    }
+}
