9eeac5c75b225cc3d760de1c0349a57494ea0ba1
thanhngan22 <nganbui9a2017@gmail.com>
2023-10-19 14:36:04 +0700

    completed layout pratice task of hw03

--- Week03/21120505/LayoutPractice/MainWindow.xaml.cs
+++ Week03/21120505/LayoutPractice/MainWindow.xaml.cs
@@ -24,5 +24,22 @@ namespace LayoutPractice
         {
             InitializeComponent();
         }
+
+        private void layoutBtn_click(object sender, RoutedEventArgs e)
+        {
+            Button btnClicked = (Button)sender;
+            string option = btnClicked.Name.ToString();
+
+            // option format: layout{id}Btn | layout1Btn/ layout2Btn...
+            Dictionary<string, object> keyValuePairs= new Dictionary<string, object>();
+            keyValuePairs.Add("layout1Btn", new Layout1());
+            keyValuePairs.Add("layout2Btn", new Layout2());
+            keyValuePairs.Add("layout3Btn", new Layout3());
+
+            // open layout window 
+            var layoutWindow = (Window) keyValuePairs[option];
+            layoutWindow.Show();
+
+        }
     }
 }
